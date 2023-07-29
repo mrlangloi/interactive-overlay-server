2047d7410681c269bcc4246627d705292a6a7603
mrlangloi <rcheung1997@gmail.com>
2023-07-29 06:24:46 -0700

    added mongoDB change streams (still testing)

--- models/images.js
+++ models/images.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
+  key: String,
   imageData: String,
   createdAt: { type: Date, default: Date.now },
+  x: Number,
+  y: Number,
+  width: Number,
+  height: Number,
+  rotation: Number,
+  opacity: Number,
 });
 
 const Image = mongoose.model('image', imageSchema);
--- server.js
+++ server.js
@@ -8,42 +8,90 @@ const Image = require('./models/images.js');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  }
+});
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000", // restrict calls to those this address
+}));
 
 /**
  * TODO
- * - I've successfully stored image data to mongoDB Atlas
- * - now I need to translate that data back into an image for the frontend
- * - still need to find a way to connect OBS websocket to my server
+ * - I'm able to get the image data from mongoDB
+ * - Now I need to find a way to simultaneously update the positional data across all clients
  * - find a way to rotate images
  */
 
+
+// Get all images from mongoDB
+app.get('/images', async (req, res) => {
+  try {
+    const images = await Image.find();
+    res.json(images);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // Connect to MongoDB
 mongoose.connect(`${process.env.MONGO_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then( () => {
-  console.log('connected to mongoDB');
+  console.log('Connected to mongoDB');
 }).catch( err => {
   console.log(err);
 });
 
-// For debugging if mongoose is unable to connect to mongoDB
-const db = mongoose.connection;
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+const connection = mongoose.connection;
 
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+  const imagesChangeStream = connection.collection('images').watch();
+  
+  imagesChangeStream.on('change', (change) => {
+    console.log(change);
+    if (change.operationType === 'insert') {
+      const image = {
+        _id: change.fullDocument._id,
+        key: change.fullDocument.key,
+        imageData: change.fullDocument.imageData,
+        x: change.fullDocument.x,
+        y: change.fullDocument.y,
+        width: change.fullDocument.width,
+        height: change.fullDocument.height,
+        rotation: change.fullDocument.rotation,
+        opacity: change.fullDocument.opacity,
+      };
+      io.emit('newImage', image);
+    } 
+    else if (change.operationType === 'update') {
+      const image = {
+        _id: change.documentKey._id,
+        key: change.updateDescription.updatedFields.key,
+        imageData: change.updateDescription.updatedFields.imageData,
+        x: change.updateDescription.updatedFields.x,
+        y: change.updateDescription.updatedFields.y,
+        width: change.updateDescription.updatedFields.width,
+        height: change.updateDescription.updatedFields.height,
+        rotation: change.updateDescription.updatedFields.rotation,
+        opacity: change.updateDescription.updatedFields.opacity,
+      };
+      io.emit('updatedImage', image);
+    }
+    else if (change.operationType === 'delete') {
+      io.emit('deletedImage', change.documentKey.key);
+    }
+  });
+  
+});
 
 // Socket.io connection
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log(`User ${socket.id} connected`);
 
   // Handle when a client sends a message
   socket.on('message', message => {
@@ -51,28 +99,28 @@ io.on('connection', (socket) => {
     io.emit('message', message);
   })
 
-  // Handle 'previewImage' event from the client
-  socket.on('previewImage', (imageData) => {
-    // Broadcast the image data to all connected clients (except the sender)
-    socket.broadcast.emit('newImage', imageData);
-  });
-
   socket.on('uploadImage', (data) => {
     try {
       // Store the image data in MongoDB
-      const image = new Image({ imageData: data });
+      const image = new Image({ key: data.key, imageData: data.imageData, x: data.x, y: data.y, width: data.width, height: data.height, rotation: data.rotation, opacity: data.opacity });
       image.save().then( () => {
-        // Broadcast the image data to all connected clients
+        // // Broadcast the image data to all connected clients
+        console.log(data);
         socket.broadcast.emit('newImage', data);
+        console.log('Image saved to MongoDB');
       });
     } catch (error) {
       console.error('Error saving image:', error);
     }
   });
 
+  socket.on('updateImage', (data) => {
+    socket.broadcast.emit('updatedImage', data);
+  });
+
   // Handle when a client disconnects
   socket.on('disconnect', () => {
-    console.log('A user disconnected.');
+    console.log(`User ${socket.id} disconnected.`);
   });
 });
 
